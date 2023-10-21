f337ad7be1562c7b56936801c5b62a0a3ca67558
Jakub Stibůrek <jakub@stiburek.dev>
2023-10-21 23:53:39 +0200

    feat: logout, dashboard grid, theme

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {tap} from "rxjs";
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
+  async logout(event: Event) {
+    event.preventDefault();
+
+    this.http.post<any>(`${environment.apiUrl}/auth/logout`, {}, {observe: 'response'})
+
+      .pipe(
+        tap((response: HttpResponse<any>) => {
+          if (response.status === 201) {
+            this.router.navigate(['/login']);
+          } else {
+            alert('Logout failed');
+          }
+        })
+      )
+      .subscribe();
+  }
 }
